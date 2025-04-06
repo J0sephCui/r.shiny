1e53c19490a28119e5a9430fe48d3ebff46ce038
J0sephCui <scui0743@uni.sydney.edu.au>
2025-04-06 16:30:16 +1000

    test

--- server.R
+++ server.R
@@ -1,18 +1,16 @@
 # Mock Storage do not need configuration details but does require mock data
 # When using an Azure SQL Server, configuration details are needed
 # Configuration details can be stored in .Reviron file and retrieved via Environment Package
+
 configuration <- data.frame()
 
-storage <- configuration |> Storage::Storage(type = "memory")
-Todo.Mock.Data |> storage[['seed.table']]('Todo')
-
-# configurator <- Storage::ODBC.Configurator()
-# configuration <- configurator[['get.config']](type = 'Preset')
-# storage <- configuration |> Storage::Storage()
-
-# Data Layer
+configurator <- Storage::ODBC.Configurator()
+configuration <- configurator[['get.config']](type = 'Preset')
+storage <- configuration |> Storage::Storage()
+# # Data Layer
 data  <- storage |> Todo.Orchestration()
 
+
 shinyServer(\(input, output, session) {
-  Todo.Controller("todo", data)
+  Todo.Controller("Todo", data)
 })
